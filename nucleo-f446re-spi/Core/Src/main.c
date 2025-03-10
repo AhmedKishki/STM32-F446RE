cdaec6f7fbd3f2fb54c7998d886ab9eeb5cf685e
delta-ahmed <alaa6394@gmail.com>
2025-03-10 18:13:51 +0100

    new

--- nucleo-f446re-spi/Core/Src/main.c
+++ nucleo-f446re-spi/Core/Src/main.c
@@ -9,6 +9,27 @@ void setAlternateFunction(void);
 void configSPI(void);
 uint8_t transferSPI(uint8_t tx_data);
 
+
+int main(void){
+	// MPU9250 ADDRESS
+	uint8_t data[4] = {187, 188, 189, 190};
+
+	// STORE DATA RECEIVED FROM MPU)9250
+	uint8_t rxd = 0;
+
+	// SET UP TIMER
+	initTim2();
+
+	// SETUP SPI MASTER
+	initSPI();
+
+	while(1){
+		// WRITE TO SPI
+		rxd = transferSPI();
+	}
+}
+
+
 void initClocks(void){
     //
     // Clock initialization is required to enable peripheral hardware blocks:
@@ -17,40 +38,72 @@ void initClocks(void){
     //
 
     // Enable clocks for GPIO ports (AHB1 peripheral bus)
-    RCC->AHB1ENR |= ((1u << 0)       // Enable GPIOA clock
-                    |(1u << 1));     // Enable GPIOB clock
+    RCC->AHB1ENR |= ((1 << 0)       // Enable GPIOA clock
+                    |(1 << 1));     // Enable GPIOB clock
 
     // Enable clock for SPI1 peripheral (APB2 peripheral bus)
-    RCC->APB2ENR |= (1u << 12);      // SPI1 clock enabled (bit 12)
+    RCC->APB2ENR |= (1 << 12);      // SPI1 clock enabled (bit 12)
+}
+
+uint8_t transferSPI(uint8_t tx_data){
+	uint8_t rx_data = 0;
+
+	// Enable SPI
+	SPI1->CR1 |= (1 << 6);
+
+	// Write data and dummy byte to data register
+	SPI1->DR = (uint16_t)(tx_data << 8);
+
+	// Wait until SPI is not BUSY and RX BUFFER is not empty
+	while(((SPI1->SR) & (1 << 7)) || (!((SPI1->SR) & (1 << 0))));
+
+	// Read a byte from the RX BUFFER
+	rx_data = (uint8_t)SPI1->DR;
+
+	// Disable SPI
+	SPI1->CR1 &= ~(1 << 6);
+
+	return rx_data;
 }
 
 void configSPI(void){
-	// Clear Bits
-	SPI1->CR1 &= ~(
-	              (1 << 15)  // BIDIMODE: Set Full Duplex mode (0 = Full Duplex, 1 = Half Duplex)
-	             |(1 << 13)  // CRCEN: Disable CRC (0 = CRC Calculation Disabled)
-				 |(1 << 11)	 // DFF: clear
-	             |(1 << 10)  // RXONLY: Disable Simplex mode (0 = Full Duplex)
-	             |(1 << 9)   // SSM: Disable Software Slave Management (0 = Hardware NSS management)
-	             |(1 << 7)   // LSBFIRST: MSB first (0 = MSB first, 1 = LSB first)
-	             |(7 << 3)   // BR[2:0]: Clear baud rate control bits (used for SPI clock speed division)
-	             );
-	SPI1->CR2 &= ~(
-            	  (7 << 5)	 // Clear Interrupt Bits
-				 |(1 << 4) 	 // SPI in Motorola format
-				 |(1 << 3) 	 // No consecutive transfer
-				 |(3 << 0)	 // No DMA
-				 );
-	// Set Bits
-	SPI1->CR1 |= (
-				  (1 << 11)	 // DFF: 16 bit data transfer option
-	             |(5 << 3)   // BR[2:0]: Set SPI clock prescaler to divide by 64
-	             |(1 << 2)   // MSTR: Enable Master mode
-	             |(1 << 1)   // CPOL: Set Clock Polarity to 1 (Idle high)
-	             |(1 << 0)   // CPHA: Set Clock Phase to 1 (Capture on second clock transition)
-	             );
-	SPI1->CR2 |= ((1 << 2)); // SSOE enabled
+    // Step 1: Configure GPIO for MOSI, MISO, and SCK pins
+    // (This step should be done separately in GPIO configuration code)
 
+    // Step 2: Write to the SPI_CR1 register
+    // Clearing specific bits to reset settings before configuration
+    SPI1->CR1 &= ~(
+                  (1 << 15)  // BIDIMODE: Set Full Duplex mode (0 = Full Duplex, 1 = Half Duplex)
+                 |(1 << 13)  // CRCEN: Disable CRC (0 = CRC Calculation Disabled)
+                 |(1 << 11)  // DFF: Clear Data Frame Format (Configuring below)
+                 |(1 << 10)  // RXONLY: Disable Simplex mode (0 = Full Duplex)
+                 |(1 << 9)   // SSM: Disable Software Slave Management (0 = Hardware NSS management)
+                 |(1 << 7)   // LSBFIRST: Set MSB First (0 = MSB first, 1 = LSB first)
+                 |(7 << 3)   // BR[2:0]: Clear baud rate control bits (used for SPI clock speed division)
+                 );
+
+    // Step 3: Write to SPI_CR2 register
+    SPI1->CR2 &= ~(
+                  (7 << 5)   // Clear Interrupt Bits
+                 |(1 << 4)   // SPI in Motorola format (0 = Motorola, 1 = TI)
+                 |(1 << 3)   // No consecutive transfer (0 = Standard SPI)
+                 |(3 << 0)   // No DMA (DMA is not used)
+                 );
+
+    // Setting specific configuration bits
+    SPI1->CR1 |= (
+                  (1 << 11)  // DFF: 16-bit data transfer mode (Step 2h)
+                 |(5 << 3)   // BR[2:0]: Set SPI clock prescaler to divide by 64 (Step 2a)
+                 |(1 << 2)   // MSTR: Enable Master mode (Step 2g)
+                 |(1 << 1)   // CPOL: Clock Polarity = 1 (Idle High) (Step 2b)
+                 |(1 << 0)   // CPHA: Clock Phase = 1 (Capture on second edge) (Step 2b)
+                 );
+
+    // Step 3a: Configure SSOE (Slave Select Output Enable)
+    SPI1->CR2 |= (1 << 2); // Enable SSOE (Step 3a)
+
+    // Steps 4 and 5 (CRC polynomial and DMA configuration) are not included
+    // since CRC and DMA are not used in this configuration.
 }
 
 void setPinMode(void){
